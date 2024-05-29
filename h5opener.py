12b34a71f83e7f456e4b66ac435853aab37528fd
HeineCantor <carmine.palmese@gmail.com>
2024-05-29 14:18:43 +0200

    clean-up + add notebooks

--- h5opener.py
+++
@@ -1,8 +0,0 @@
-import h5py
-
-PATH = '/home/heinecantor/Desktop/MIDI Dataset/lmd_matched_h5/A/A/A/TRAAAGR128F425B14B.h5'
-#PATH = '/home/heinecantor/Desktop/MIDI Dataset/msd_summary_file.h5'
-
-file = h5py.File(PATH, 'r')
-print(file.keys())
-print(list(file['metadata']))
